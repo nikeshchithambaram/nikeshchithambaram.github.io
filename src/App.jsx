6341ed1f00c1185d41572a6e6eacb8b4fcceb555
Nikesh <42144202+cnikesh@users.noreply.github.com>
2025-09-02 14:02:37 -0400

    updated project layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import HeaderLeft from './components/HeaderLeft'
+import Home from './components/Home'
+import Skills from './components/Skills'
+import Experience from './components/Experience'
+import Contact from './components/Contact'
+
 function App() {
   return (
     <>
-      <div>this project</div>
+      <div className="flex min-h-screen w-full subpixel-antialiased">
+        <HeaderLeft/>
+        <main className="flex-[3]">
+          <Home/>
+          <Experience/>
+          <Skills/>
+          <Contact/>
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/HeaderLeft.jsx
@@ -0,0 +1,8 @@
+
+const HeaderLeft = () => {
+  return (
+    <div>HeaderLeft</div>
+  )
+}
+
+export default HeaderLeft
